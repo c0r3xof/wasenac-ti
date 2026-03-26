294358d631272c25535e5d297e0cff753e8e8ff5
c0r3xof <c0r3xof@gmail.com>
2026-03-26 09:40:50 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
