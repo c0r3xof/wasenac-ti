77a93db3005ab8dd7e7e2565aa9d639bc7575ece
c0r3xof <c0r3xof@gmail.com>
2026-03-31 10:26:19 -0300

    Update logo image source in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,8 +13,10 @@ export default function RootPage() {
     <div className="min-h-screen bg-[#f0f2f5] flex flex-col items-center justify-center gap-3">
       {/* O ?v=1 serve para limpar o cache do navegador e mostrar o ícone novo */}
       <img 
-        src="/android-chrome-512x512.png"
+        src="/navegador-1024x1024.png"
         alt="Logo WaSenac-Ti" 
+        className="w-[300px] h-[300px] animate-pulse object-contain"
+/>
       />
       
       <p className="text-gray-400 animate-pulse font-sans">
